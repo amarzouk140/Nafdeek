33e061382e4d2416f2dc9dd95d2edf15e267f8fe
Ayman <amarzouk@student.42abudhabi.ae>
2024-05-13 00:48:46 +0400

    update

--- MOI_APP/lib/main.dart
+++ MOI_APP/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 void main() {
   runApp(MyApp());
 }
@@ -23,8 +24,8 @@ final ThemeData appTheme = ThemeData(
   ),
   visualDensity: VisualDensity.adaptivePlatformDensity,
   textTheme: TextTheme(
-    headline6: TextStyle(color: Colors.white, fontSize: 20),  // For AppBar titles
-    bodyText2: TextStyle(color: Colors.black, fontSize: 18),  // Main body text
+    titleLarge: TextStyle(color: Colors.white, fontSize: 20),  // For AppBar titles
+    bodyMedium: TextStyle(color: Colors.black, fontSize: 18),  // Main body text
   ),
   iconTheme: IconThemeData(
     color: Colors.white,  // AppBar icon color
@@ -110,16 +111,36 @@ class IntroductionScreen extends StatelessWidget {
 class MainApplicationScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    double iconSize = 35;  // Adjusted icon size
+
     return Scaffold(
-      body: Container(
-        width: MediaQuery.of(context).size.width, // Ensure it fills the width
-        height: MediaQuery.of(context).size.height, // Ensure it fills the height
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/images/moi_final.jpg'),
-            fit: BoxFit.cover, // This ensures the image covers the full screen
+      body: Stack(
+        children: <Widget>[
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/moi_final.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
           ),
-        ),
+          Positioned(
+            top: MediaQuery.of(context).padding.top -5 ,  // Adjust for better positioning
+            left: 275,  // Aligning the icon on the right side
+            child: IconButton(
+              icon: Image.asset('assets/images/logo2.png', width: iconSize, height: iconSize),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => NafdeekPage()),
+                );
+              },
+            ),
+          ),
+          // Other widgets can be added here if needed
+        ],
       ),
     );
   }
@@ -252,3 +273,308 @@ class CustomizedPage extends StatelessWidget {
   }
 }
 
+
+class VolunteerProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Example data - in a real app, this should come from a data source or state management
+    int numberOfTasksCompleted = 42;  // Example number of tasks completed
+    int volunteerPoints = 1230;       // Example points earned by the volunteer
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Volunteer Profile"),
+        backgroundColor: Color(0xFF013A6B), // Consistent theme color
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.edit),
+            onPressed: () {
+              // Implement action, for example, navigate to edit profile page
+            },
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 20),
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage('assets/images/volunteer.png'), // Example profile image
+              backgroundColor: Colors.grey[200],
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  'Ayman Marzouk',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(width: 5),
+                Icon(Icons.verified, color: Color(0xFF013A6B)), // Verified icon next to the name
+              ],
+            ),
+            Text(
+              'Certified Volunteer',
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.grey[700],
+              ),
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: List.generate(5, (index) => Icon(Icons.star, color: Colors.amber)),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.assignment_turned_in, color: Color(0xFF013A6B)),
+                SizedBox(width: 10),
+                Text('Tasks Completed: $numberOfTasksCompleted'),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.emoji_events, color: Color(0xFF013A6B)),
+                SizedBox(width: 10),
+                Text('Points: $volunteerPoints'),
+              ],
+            ),
+            SizedBox(height: 20),
+            Card(
+              margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: ListTile(
+                leading: Icon(Icons.email, color: Color(0xFF013A6B)),
+                title: Text('amarzouk@student.42abudhabi.ae'),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: ListTile(
+                leading: Icon(Icons.phone, color: Color(0xFF013A6B)),
+                title: Text('+971 52 4802292'),
+              ),
+            ),
+            // Card(
+            //   margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            //   child: ListTile(
+            //     leading: Icon(Icons.verified_user, color: Color(0xFF013A6B)),
+            //     title: Text('Certified Volunteer'),
+            //   ),
+            // ),
+            SizedBox(height: 20),
+            ElevatedButton.icon(
+              icon: Icon(Icons.add_circle_outline, color: Colors.white),
+              label: Text('Start New Task'),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xFF013A6B), // Button background color
+                foregroundColor: Colors.white, // Button text and icon color
+                padding: EdgeInsets.symmetric(vertical: 12, horizontal: 24),
+              ),
+              onPressed: () {
+                // Implement action to start a new task
+              },
+            ),
+            SizedBox(height: 20),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+class NafdeekPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("NAFDEEK Service"),
+        backgroundColor: Color(0xFF013A6B),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Expanded(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFF013A6B),
+                    foregroundColor: Colors.white,
+                    padding: EdgeInsets.symmetric(vertical: 20),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  onPressed: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => RequestServicePage()),
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Image.asset('assets/images/logo2.png', width: 60, height: 60),
+                      SizedBox(height: 10),
+                      Text('Request for NAFDEEK', textAlign: TextAlign.center),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(width: 20), // Space between the buttons
+              Expanded(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFF013A6B),
+                    foregroundColor: Colors.white,
+                    padding: EdgeInsets.symmetric(vertical: 20),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  onPressed: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => VolunteerProfilePage()),
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Image.asset('assets/images/icon.png', width: 60, height: 60),
+                      SizedBox(height: 10),
+                      Text('Volunteer Page', textAlign: TextAlign.center),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class RequestServicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Sample data for demonstration
+    List<Map<String, dynamic>> volunteers = [
+      {
+        'name': 'Amna Alnaqbi',
+        'rating': 5,
+        'distance': '1 km away',
+        'verified': true,
+        'image': 'assets/images/volunteer1.png',
+      },
+      {
+        'name': 'Mahmod Sayed',
+        'rating': 4,
+        'distance': '5 km away',
+        'verified': true,
+        'image': 'assets/images/volunteer2.png',
+      },
+      {
+        'name': 'Ahmed Alshafy',
+        'rating': 3,
+        'distance': '1 km away',
+        'verified': true,
+        'image': 'assets/images/volunteer.png',
+      },
+      {
+        'name': 'Sumyah Helal',
+        'rating': 5,
+        'distance': '3 km away',
+        'verified': true,
+        'image': 'assets/images/volunteer4.png',
+      },
+      {
+        'name': 'Mohamed Alshamsi',
+        'rating': 5,
+        'distance': '2.5 km away',
+        'verified': false,
+        'image': 'assets/images/volunteer5.png',
+      },
+      {
+        'name': 'Sara Almazrouei',
+        'rating': 4,
+        'distance': '4 km away',
+        'verified': false,
+        'image': 'assets/images/volunteer4.png',
+      },
+      {
+        'name': 'Khalid Alhammadi',
+        'rating': 4,
+        'distance': '4 km away',
+        'verified': false,
+        'image': 'assets/images/volunteer5.png',
+      }
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Request Service"),
+        backgroundColor: Color(0xFF013A6B),
+      ),
+      body: ListView.separated(
+        itemCount: volunteers.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              leading: CircleAvatar(
+                backgroundImage: AssetImage(volunteers[index]['image']),
+              ),
+              title: Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      volunteers[index]['name'],
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(fontSize: 16),  // Adjusted font size for the name
+                    ),
+                  ),
+                  if (volunteers[index]['verified'])
+                    Icon(Icons.verified, color: Color(0xFF013A6B), size: 14),
+                ],
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: Row(
+                  children: <Widget>[
+                    ...List.generate(5, (i) => Icon(
+                      i < volunteers[index]['rating'] ? Icons.star : Icons.star_border,
+                      color: Colors.amber,
+                      size: 16,  // Adjusted star size
+                    )),
+                    SizedBox(width: 10),
+                    Text(
+                      volunteers[index]['distance'],
+                      style: TextStyle(fontSize: 12),  // Reduced font size for distance
+                    ),
+                  ],
+                ),
+              ),
+              trailing: ElevatedButton(
+                onPressed: () {
+                  // Action to send a request to this volunteer
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFF013A6B),  // Button background
+                  foregroundColor: Colors.white,  // Button foreground
+                ),
+                child: Text('Send a Request', style: TextStyle(fontSize: 12)),  // Adjusted font size
+              ),
+            ),
+          );
+        },
+        separatorBuilder: (context, index) => SizedBox(height: 10),  // Space between each card
+      ),
+    );
+  }
+}
